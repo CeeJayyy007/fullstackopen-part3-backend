9ebbd00e9ac7217a5afc98818ba3985d7ab9c807
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-10-26 04:23:17 -0400

    complete 3.2: phonebook backend step 2

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 // persons data
 let persons = [
-  [
-    {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456",
-    },
-    {
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-    },
-    {
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345",
-    },
-    {
-      id: 4,
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-    },
-  ],
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ];
 
-// get persons resource
+// get all persons
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+// get locale time and convert to string
+const getTorontoTimeISOString = () => {
+  const now = new Date();
+  const torontoTime = new Date(
+    now.toLocaleString("en-US", { timeZone: "America/Toronto" })
+  );
+  return torontoTime;
+};
+
+// get person count info
+app.get("/api/info", (request, response) => {
+  const personCount = persons.length;
+
+  const message = `<p>Phonebook has info for ${personCount} people</p>`;
+  const torontoTimeString = getTorontoTimeISOString();
+
+  if (personCount) {
+    response.send(`${message} ${torontoTimeString}`);
+  } else {
+    response.status(404).end();
+  }
 });
 
 const PORT = 3001;
