c59b3b36d8b8e42fc4b2d06708c464ae5d16eeae
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-10-30 03:27:46 -0400

    complete 3.19: phonebook database step 7

--- index.js
+++ index.js
@@ -66,14 +66,13 @@ app.get("/api/info", (request, response, next) => {
 
 // update person record
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -83,18 +82,6 @@ app.put("/api/persons/:id", (request, response, next) => {
 // add person
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
-
-  if (body.name === undefined) {
-    return response.status(400).json({
-      error: "name missing",
-    });
-  }
-
-  if (body.number === undefined) {
-    return response.status(400).json({
-      error: "number missing",
-    });
-  }
 
   const person = new Person({
     name: body.name,
@@ -129,8 +116,11 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  } else if (error.number === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
-
   next(error);
 };
 
